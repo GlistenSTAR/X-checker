8ca6d969cb2e8d1548ed9710cec0ae7b729f1219
GlistenSTAR <glistenstar00@gmail.com>
2022-02-14 02:00:30 -0600

    move the extensio on extension folder and check the data_cheker

--- data_checker.py
+++ data_checker.py
@@ -9,26 +9,29 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 
+
 def get_event_name_and_date(url):
     if 'axs.' in url:
         return check_axs(url)
 
+
 def make_request(url):
     headers = {
-        # 'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36.',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0 Mozilla/5.0 (Macintosh; Intel Mac OS X x.y; rv:42.0) Gecko/20100101 Firefox/42.0.'
+        'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36.',
     }
     r = requests.get(url, headers=headers)
     if r.status_code == 200 or r.status_code == 506:
         return BeautifulSoup(r.content, 'html.parser')
     print(r.status_code, url)
 
+
 def check_axs(url):
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
-    user_agent='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36.'
+    chrome_options.add_experimental_option(
+        'excludeSwitches', ['enable-logging'])
+    user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36.'
     chrome_options.add_argument('--user-agent=%s' % user_agent)
-    # chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--headless')
     while True:
         try:
             driver = webdriver.Chrome(
@@ -48,12 +51,12 @@ def check_axs(url):
     driver.find_element_by_css_selector('input[name="q"]')
     time.sleep(1)
 
-
     driver.get(url)
-    time.sleep(3)
+    time.sleep(2)
     try:
         element = WebDriverWait(driver, 5).until(
-            EC.presence_of_element_located((By.XPATH, '//*[@class="btn--single btn btn-default"]'))
+            EC.presence_of_element_located(
+                (By.XPATH, '//*[@class="btn--single btn btn-default"]'))
         )
     except:
         try:
@@ -67,36 +70,33 @@ def check_axs(url):
     time.sleep(2)
     if flag:
         try:
-            driver.find_element_by_xpath('//*[@class="btn--single btn btn-default"]').click()
+            driver.find_element_by_xpath(
+                '//*[@class="btn--single btn btn-default"]').click()
         except:
             driver.find_element_by_xpath('//button[@id="ackBtn"]').click()
         time.sleep(3)
         soup = BeautifulSoup(driver.page_source, 'html.parser')
         try:
-            name = soup.find('div', {'class':'event-summary-headline__event'}).text.strip()
+            name = soup.find(
+                'div', {'class': 'event-summary-headline__event'}).text.strip()
         except:
-            name = soup.find('h1', {'class':'c-marquee__headliner h-ellipsis'}).find_next('a').text.strip()
+            name = soup.find(
+                'h1', {'class': 'c-marquee__headliner h-ellipsis'}).find_next('a').text.strip()
         try:
             venue = soup.find('span', {'class': 'venue'}).text.strip()
         except:
-            venue = soup.find('div', {'class': 'c-event-info__venue c-event-info__venue--sticky-nav'}).text.strip()
+            venue = soup.find('div', {
+                              'class': 'c-event-info__venue c-event-info__venue--sticky-nav'}).text.strip()
         name = name + "-/-" + venue
-        try: 
-            date = soup.find('span', {'class':'event-header-date'}).text.strip()
+        try:
+            date = soup.find(
+                'span', {'class': 'event-header-date'}).text.strip()
             date = parser.parse(date.split('–')[0]).strftime('%Y-%m-%d')
         except:
-            date = soup.find('span', {'class':'c-event-info__table-date'}).text.strip()
+            date = soup.find(
+                'span', {'class': 'c-event-info__table-date'}).text.strip()
             date = parser.parse(date.split('–')[0]).strftime('%Y-%m-%d')
-    # print(f'date>>>>>>>{date}')
-    # time.sleep(3000)
     driver.quit()
     print("Add the Event on Google sheet.....")
     return name, date
 
-# for url in ['https://basscanyonfestival.frontgatetickets.com/event/i8co6kruzz405we8',
-#             'https://www.ticketfly.com/purchase/event/1803134/tfly',
-#             'https://www.eventbrite.com/e/casablanca-the-montalban-rooftop-movies-tickets-54783106747?aff=erelexpmlt#tickets',
-#             'https://www.etix.com/ticket/p/3379234/hartmut-sauers-musikkabinettduosassoninocturne-wandel-durch-die-nacht-dresdensch%C3%B6nfeld-deutschlands-zauberschloss?partner_id=430',
-#             'https://www.ticketweb.com/event/lords-of-acid-with-special-brick-by-brick-tickets/9072935?REFERRAL_ID=twflash']:
-#     print('for url', url)
-#     print(get_event_name_and_date(url))
